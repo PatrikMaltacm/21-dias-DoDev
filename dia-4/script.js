40dd87e6b744ce106ec682b199ad03c817208fa3
Patrik Malta <maltapatrik3@gmail.com>
2023-11-18 20:39:06 -0300

    condições

---
+++ dia-4/script.js
@@ -0,0 +1,10 @@
+let idade = Number(prompt("Insira sua idade: "));
+
+// == igual | != diferente | < menor que | > maior que | <= >= menor ou maior igual 
+// === igual valor e tipo | !== diferente valor e tipo 
+
+if (idade <= 10){
+    console.log("Voceê tem menos ou tem 10 anos.");
+}else{
+    console.log("Você tem mais que 10 anos.");
+}
