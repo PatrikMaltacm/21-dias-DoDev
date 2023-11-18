f4883df0c16f685bc8b3198a65fa8f0a0f79f35a
Patrik Malta <maltapatrik3@gmail.com>
2023-11-18 20:26:59 -0300

    exercicio de captura de dados calculo de ano de nascimento e imc

--- dia-3/script.js
+++ dia-3/script.js
@@ -1,8 +1,10 @@
-let entradaDoUsuario = prompt("Qual sua idade?");
-console.log(entradaDoUsuario);
-let entradaDoUsuarioEmNumber = Number(entradaDoUsuario);
-console.log(entradaDoUsuarioEmNumber);
+let userName = prompt("Qual seu nome?");
+let oldUser = prompt("Qual sua idade");
+let userHeight = prompt("Qual sua altura");
+let userWeight = prompt("Qual seu peso?");
 
+let yearOfBirth = 2023 - parseInt(oldUser);
 
-//parseInt converte para int
-//Number converte para numero
+let imcUser = userWeight / (userHeight * userHeight);
+
+console.log("Olá " + userName + " você tem " + oldUser + " anos, nasceu em " + yearOfBirth + ", tem " + userHeight + " de altura, pesa " + userWeight + "kg" + " e seu IMC é de " + imcUser + " kg/m2."); 
