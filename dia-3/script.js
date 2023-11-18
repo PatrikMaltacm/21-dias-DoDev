98af7292bc8b01fc4f8696da0a4e5168f8e6914f
Patrik Malta <maltapatrik3@gmail.com>
2023-11-18 19:46:58 -0300

    usando variaveis e constantes

---
+++ dia-3/script.js
@@ -0,0 +1,9 @@
+let meuNome = "Patrik Malta"
+console.log(meuNome)
+meuNome = "Patrik Carvalho"
+console.log(meuNome)
+
+console.log("============= const =============")
+
+const num = 2
+console.log(num)
