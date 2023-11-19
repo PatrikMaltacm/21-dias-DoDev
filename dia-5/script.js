62b8d1c95058062d04bed11431b093085820fe16
Patrik Malta <maltapatrik3@gmail.com>
2023-11-19 19:49:33 -0300

    exercicio de switch case 02

--- dia-5/script.js
+++ dia-5/script.js
@@ -1,22 +1,16 @@
-let num1 = Number(prompt("Digite um numero: "))
-let opc = parseInt(prompt("Qual operação deseja efetuar? " + "(1 = +) (2 = -) (3 = *) (4 = /)"))
-let num2 = Number(prompt("Digite outro numero: "))
+alert("Bem vindo ao posto Ipiranga o que deseja? ")
+let opc = parseInt(prompt("Abastecer com gasolina = (n1) " + "Abastecer com alcool = (n2) " + "Calibrar os pneus = (n3) " ))
 
-
-switch (opc){
+switch(opc){
     case 1:
-        console.log("O resultado é: " + (num1 + num2))
+        let quantGas = Number(prompt("Qual valor deseja abastecer? " + "A gasolina R$5,00 o litro"))
+        console.log("Você abasteceu: " + (quantGas / 5) + " Litros de gasolina!")
         break;
     case 2:
-        console.log("O resultado é: " + (num1 - num2))
+        let quantAlc = Number(prompt("Qual valor deseja abastecer? " + "o acool R$3,00 o litro"))
+        console.log("Você abasteceu: " + (quantAlc / 3) + " Litros de alcool")
         break;
-    case 3: 
-        console.log("O resultado é: " + (num1 * num2))
-        break;
-    case 4:
-        console.log("O resultado é: " + (num1 / num2))
-        break;
-    default:
-        console.log("Invalido");
+    case 3:
+        console.log("Pneus calibrados com sucesso!")
         break;
 }
