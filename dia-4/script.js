bd29f6ff1959c320dca89a3aa01cf98b28827038
Patrik Malta <maltapatrik3@gmail.com>
2023-11-19 18:24:52 -0300

    escopo

--- dia-4/script.js
+++ dia-4/script.js
@@ -4,7 +4,17 @@ let idade = Number(prompt("Insira sua idade: "));
 // === igual valor e tipo | !== diferente valor e tipo 
 
 if (idade <= 10){
+    console.log(idade);
+    let dentroEscopo = 'Patrik'
+    console.log(dentroEscopo);
     console.log("Voceê tem menos ou tem 10 anos.");
+    if(true){
+        console.log("estou no terceiro escopo");
+        console.log(dentroEscopo);
+    }
 }else{
     console.log("Você tem mais que 10 anos.");
 }
+
+console.log("sai do escopo");
+console.log(dentroEscopo);
