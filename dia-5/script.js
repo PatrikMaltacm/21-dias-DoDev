c290c0ad6b44bdceccc683dafe776c641a8fa190
Patrik Malta <maltapatrik3@gmail.com>
2023-11-19 19:36:24 -0300

    exercicio de switch case 01

--- dia-5/script.js
+++ dia-5/script.js
@@ -1,13 +1,22 @@
-let idade = Number(prompt("Insira sua idade: "));
+let num1 = Number(prompt("Digite um numero: "))
+let opc = parseInt(prompt("Qual operação deseja efetuar? " + "(1 = +) (2 = -) (3 = *) (4 = /)"))
+let num2 = Number(prompt("Digite outro numero: "))
 
-switch(idade){
-    case 10:
-        console.log("Você tem 10 anos.");
+
+switch (opc){
+    case 1:
+        console.log("O resultado é: " + (num1 + num2))
         break;
-    case 15:
-        console.log("Você tem 15 anos. ");
+    case 2:
+        console.log("O resultado é: " + (num1 - num2))
+        break;
+    case 3: 
+        console.log("O resultado é: " + (num1 * num2))
+        break;
+    case 4:
+        console.log("O resultado é: " + (num1 / num2))
         break;
     default:
-        console.log("Nenhuma opção corresponde.")
+        console.log("Invalido");
         break;
 }
