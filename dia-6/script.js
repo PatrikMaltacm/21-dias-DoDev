006c5cb1b7e7dafaf0043bbc6d8cd9e22ec96d69
Patrik Malta <maltapatrik3@gmail.com>
2023-11-21 07:51:32 -0300

    aprendendo for exemplo tabuada

---
+++ dia-6/script.js
@@ -0,0 +1,5 @@
+let tabuada = Number(prompt("De qual numero você quer a tabuada?"));
+
+for(let contador = 0; contador <= 10; contador++){
+    console.log(tabuada + " x " + contador + " = " + (tabuada*contador));
+}
