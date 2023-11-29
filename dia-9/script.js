48931abab8cffe1d96e2cdb3f08e9d0a70dca5df
Patrik Malta <maltapatrik3@gmail.com>
2023-11-28 21:43:25 -0300

    exercicio 03 de switch + loop + else/if

--- dia-9/script.js
+++ dia-9/script.js
@@ -1,18 +1,12 @@
-let peso = prompt("Insira seu peso: ")
-let altura = prompt("Insira seu sua altura: ")
-let idade = parseInt(prompt("Insira sua idade: "))
-let anoNas = 2023 - idade
-let imc = peso / (altura * altura)
+let anoNas = ""
+let opc = "sim"
+let idade = 0
 
-if(imc <= 18.5){
-    console.log("Sua faixa é: Magreza")
-}else if(imc > 18.5 && imc < 24.9){
-    console.log("Sua faixa é: Normal")
-}else if(imc > 24.9 && imc < 30){
-    console.log("Sua faixa é: Sobrepeso")
-}else if(imc > 30){
-    console.log("Sua faixa é: Obesidade")
+while (opc === "sim"){
+    anoNas = parseInt(prompt("Qual o ano do seu nascimento?"))
+    for(i = anoNas; i <= 2023; i++){
+        console.log(i + " - " + idade + " anos de idade");
+        idade++
+    }
+    opc = prompt("Deseja inserir outro ano? (sim ou não)")
 }
-
-console.log("Seu ano de nascimento é: " + anoNas)
-
